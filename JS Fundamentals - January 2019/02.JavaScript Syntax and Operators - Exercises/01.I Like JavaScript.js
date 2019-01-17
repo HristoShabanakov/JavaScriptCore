42bba0c6d8cfce0f2f4f1e6627475e523267edee
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-01-17 20:29:29 +0100

    JavaScript Syntax and Operators - Exercises

---
+++ JS Fundamentals - January 2019/02.JavaScript Syntax and Operators - Exercises/01.I Like JavaScript.js
@@ -0,0 +1,6 @@
+function solve(input){
+let name = input;
+console.log(`Hello ${name}, do you like JavaScript?`)
+}
+
+solve('George');
---
+++ JS Fundamentals - January 2019/02.JavaScript Syntax and Operators - Exercises/02.Even Numbers 1 to N.js
@@ -0,0 +1,13 @@
+function solve(n){
+   n = Number(n);
+
+    for (let i = 1; i <=n; i++) {
+        
+        if(i % 2 ==0){
+            console.log(i);
+        }
+    }
+    
+}
+
+solve(7);
