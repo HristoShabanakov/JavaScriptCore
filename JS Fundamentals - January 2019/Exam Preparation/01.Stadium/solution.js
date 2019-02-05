802a00d80196fd533fb6e2b54f67738b31f09894
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-02-05 23:15:08 +0100

    Getting buttons value in array.

--- JS Fundamentals - January 2019/Exam Preparation/01.Stadium/solution.js
+++ JS Fundamentals - January 2019/Exam Preparation/01.Stadium/solution.js
@@ -1,22 +1,7 @@
 function solve() {
-    window.onload=function(){
-        let targetDiv = document.getElementById("Levski").getElementsByClassName("seat")[0];
-        targetDiv.textContent = "Hello";
-    let seat = document.getElementsByClassName("seat")[0].className;
-    let levski = document.getElementById("exercise").className;
-    seat.onclick
-    console.log(levski);
+    //button.seat
+    let btns = Array.from(document.getElementsByTagName('button'));
 
-    let seatElement = document.getElementById('seat');
-    seatElement.addEventListener('click', buttonClick);
-
-        let but = document.querySelector('exercise.Levski.seat').value;
-        console.log(but);
-    
-
-    buttonClick.addEventListener('click', () => {
-        let btn = document.getElementById('seat').value;
-        console.log(seatElement);
-    });
-   }
+    let output = document.getElementsByTagName('textarea')[0];
+    console.log(output);
 }
