802495db123c42ca6bccacbf4cc4bace23acdd85
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-02-06 22:54:33 +0100

    Adding solution for Stadium problem.

--- JS Fundamentals - January 2019/Exam Preparation/01.Stadium/solution.js
+++ JS Fundamentals - January 2019/Exam Preparation/01.Stadium/solution.js
@@ -1,7 +1,53 @@
 function solve() {
     //button.seat
-    let btns = Array.from(document.getElementsByTagName('button'));
+    let buttons = Array.from(document.getElementsByClassName('seat'));
+    let output = document.getElementById('output');
+    let summary = document.getElementById('summary');
 
-    let output = document.getElementsByTagName('textarea')[0];
-    console.log(output);
+    let obj = {
+        "Levski": {
+            'A': 10,
+            'B': 7,
+            'C' : 5
+        },
+        "Litex": {
+            'A': 10,
+            'B': 7,
+            'C' : 5
+        },
+        "VIP":{
+            'A': 25,
+            'B': 15,
+            'C' : 10
+        },
+        '__summary__':{
+            'fans': 0,
+            'totalProfit' : 0
+        }
+};
+
+    Array.from(buttons).forEach((btn) =>{
+        btn.addEventListener('click',clickEvent);
+    });
+
+    summary.children[0].addEventListener('click', getTheSummary);
+
+    function clickEvent(e){
+        let seat = e.target;
+        let zone = seat.parentNode.parentNode.parentNode.parentNode.parentNode.className;
+        let sector = String.fromCharCode(65 + e.target.parentNode.cellIndex);
+
+        if(seat.style.backgroundColor === ''){
+            seat.style.backgroundColor = "rgb(255,0,0)";
+            obj.__summary__.fans += 1;
+            obj.__summary__.totalProfit += obj[zone][sector];
+            output.value += `  Seat ${seat.textContent} in zone ${zone} sector ${sector} was taken.\n`
+        }else{
+            output.value += `  Seat ${seat.textContent} in zone ${zone} sector ${sector} is unavailable.\n`
+        }
+    }
+
+    function getTheSummary(){
+        summary.children[1].textContent = `${obj.__summary__.totalProfit} leva, ${obj.__summary__.fans} fans. `;
+    }
 }
