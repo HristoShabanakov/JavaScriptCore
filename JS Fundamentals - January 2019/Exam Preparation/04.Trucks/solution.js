45a7350a79f7af2b352d7f279ef1d427ff7d6b3e
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-02-07 23:18:06 +0100

    Adding extra functionality.

--- JS Fundamentals - January 2019/Exam Preparation/04.Trucks/solution.js
+++ JS Fundamentals - January 2019/Exam Preparation/04.Trucks/solution.js
@@ -4,8 +4,12 @@ function solve() {
     //Get all the divs and put them in array.
    let divElements = document.getElementsByTagName('div');
     let fieldSets = document.getElementsByTagName('fieldset');
+    //Gets the first textarea present in the html
+    let output = document.querySelector('textarea');
 
-   let obj ={};
+   let obj = {
+       'backupTireSets' : []
+   };
 
    buttons[0].addEventListener('click', addNewTruck);
    buttons[1].addEventListener('click', addNewTireSet);
@@ -32,12 +36,77 @@ function addNewTruck(){
     
 }
 
+function addNewTireSet(){
+    let tires = document.getElementById('newTiresCondition').value.split(' ').map(Number);
+    obj.backupTireSets.push(tires);
+
+    let currentTireSet = createElement('div', tires.join(' '), 'tireSet');
+    let parentTruck = fieldSets[3].lastElementChild;
+    parentTruck.appendChild(currentTireSet);
+}
+
+function goToWork(){
+    let plateNumber = document.getElementById('workPlateNumber').value;
+    let distance = +document.getElementById('distance').value;
+
+    if(obj.hasOwnProperty(plateNumber)){
+        let results = areTiresGoodEnough(obj[plateNumber].tires, distance);
+
+        if(results.finalResult){
+            obj[plateNumber].distance += distance;
+            obj[plateNumber].tires = results.testedTires;
+        } else if(obj.backupTireSets.length > 0){
+            let backUpSet = obj.backupTireSets[0];
+
+            let results = areTiresGoodEnough(backUpSet, distance);
+
+            if(results.finalResult){
+                obj[plateNumber].distance += distance;
+                obj[plateNumber].tires = results.testedTires;
+                obj.backupTireSets.shift();
+            }
+
+            let truckHolder = fieldSets[3].lastElementChild;
+
+            let usedTires = document.querySelector('div.tireSet');
+
+            usedTires.remove();
+        }
+    }
+}
+
+function endOfShiftInfo(){
+    Object.keys(obj).filter((plateNumber) => plateNumber !== 'backupTireSets').forEach((plate) => {
+        output.value += `Truck ${plate} has traveled ${obj[plate].distance}.\n`
+    })
+    output.value += `You have ${obj.backupTireSets.length} sets of tires left.\n`
+
+}
+//Tires is array from all of the tires.
+function areTiresGoodEnough(tires, distance){
+let dist = distance / 1000;
+
+let result = {
+    'testedTires': [],
+    'finalResult': false
+    };
+
+    tires.forEach((tire) => {
+        result.testedTires.push(tire - dist);
+    });
+
+    if(result.testedTires.every((e) => e >= 0)){
+        result.finalResult = true; 
+    }
+
+    return result;
+}
+
 function createElement(type, text, className){
     let element = document.createElement(type);
     element.textContent = text;
     element.classList.add(className);
     return element;
 }
-
   
 }
