a814e26088bab296e47a26312a6be32b6b193fb8
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-06-03 10:53:03 +0200

    Problem 03 - Solution with Objects

---
+++ JS Fundamentals - January 2019/JS Fundamentals Retake Exam - 5 Sept 2018/03.F1 Championship.js
@@ -0,0 +1,45 @@
+function f1Championship(arr) {
+   let book = {};
+
+   for (let element of arr){
+       let tokens = element.split(' -> ');
+       let teamName = tokens[0];
+       let pilotName = tokens[1];
+       let pilotPoints = +tokens[2];
+
+       if(!book.hasOwnProperty(teamName)) {
+           book[teamName] = {
+               totalPoints: 0,
+               pilots: {}
+           };
+       }
+       if(!book[teamName]["pilots"].hasOwnProperty(pilotName)){
+           book[teamName]["pilots"][pilotName] = 0;
+       }
+
+       book[teamName]["pilots"][pilotName] +=pilotPoints;
+       book[teamName]["totalPoints"] += pilotPoints;
+   }
+
+   //Object.entries() returns an array whose elements are arrays corresponding to the enumerable
+    // string-keyed property [key, value] pairs found directly upon object.
+   let winners = Object.entries(book)
+       .sort((a,b) => b[1]["totalPoints"] - a[1]["totalPoints"]).slice(0,3);
+   
+   for (let winnerTeam of winners){
+       console.log(`${winnerTeam[0]}: ${winnerTeam[1]["totalPoints"]}`);
+
+       let sortedPilots = Object.entries(winnerTeam[1]["pilots"]).sort((a,b) => b[1] - a[1]);
+       
+       for (let pilot of sortedPilots){
+           console.log(`-- ${pilot[0]} -> ${pilot[1]}`);
+       }
+   }
+}
+
+f1Championship(["Ferrari -> Kimi Raikonnen -> 25",
+    "Ferrari -> Sebastian Vettel -> 18",
+    "Mercedes -> Lewis Hamilton -> 10",
+    "Mercedes -> Valteri Bottas -> 8",
+    "Red Bull -> Max Verstapen -> 6",
+    "Red Bull -> Daniel Ricciardo -> 4"]);
