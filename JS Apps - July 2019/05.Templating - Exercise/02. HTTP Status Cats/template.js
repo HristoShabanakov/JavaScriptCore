3010f9d35690d36ab60201b833a24f28fc23f659
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-07-29 21:57:11 +0200

    Templating Exercise - 02. HTTP Status Cats - Adding details functionality.

--- JS Apps - July 2019/05.Templating - Exercise/02. HTTP Status Cats/template.js
+++ JS Apps - July 2019/05.Templating - Exercise/02. HTTP Status Cats/template.js
@@ -10,6 +10,22 @@
         });
 
         document.getElementById('allCats').innerHTML = rendered;
+
+        let section = document.getElementById('allCats');
+        section.addEventListener('click', showMoreInfo);
+
+        function showMoreInfo(ev) {
+            if (ev.target.className === 'showBtn') {
+
+                if (ev.target.textContent === 'Hide status code') {
+                    ev.target.textContent = 'Show status code';
+                    ev.target.nextElementSibling.style.display = 'none';
+                } else {
+                    ev.target.textContent = 'Hide status code';
+                    ev.target.nextElementSibling.style.display = 'block';
+                }
+            }
+        }
     }
 
 })()
